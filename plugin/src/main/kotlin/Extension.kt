3bccd97555743101845814f0daccab0767be4a3b
itsusinn <itsusinn@foxmail.com>
2021-10-17 15:14:32 +0800

    feat: some helper functions

---
+++ plugin/src/main/kotlin/Extension.kt
@@ -0,0 +1,34 @@
+package org.meowcat.mesagisto.mirai
+
+import com.luciad.imageio.webp.WebPReadParam
+import kotlinx.coroutines.runInterruptible
+import org.tinylog.kotlin.Logger
+import java.io.File
+import javax.imageio.ImageIO
+import javax.imageio.stream.FileImageInputStream
+
+@JvmName("isWebp-ext")
+suspend fun File.isWebp() = isWebp(this)
+
+suspend fun isWebp(file: File): Boolean = runInterruptible fn@{
+  Thread.currentThread().contextClassLoader = Plugin::class.java.classLoader
+  val iis = ImageIO.createImageInputStream(file)
+  val ir = ImageIO.getImageReaders(iis)
+  val next = ir.next()
+  return@fn when (next.formatName) {
+    "WebP" -> true
+    else -> false
+  }
+}
+suspend fun convertWebpToPng(from: File, to: File) = runInterruptible {
+  Thread.currentThread().contextClassLoader = Plugin::class.java.classLoader
+  Logger.trace { "Converting" }
+  val reader = ImageIO.getImageReadersByMIMEType("image/webp").next()
+  val readParam = WebPReadParam().apply {
+    isBypassFiltering = true
+  }
+  reader.input = FileImageInputStream(from)
+  val image = reader.read(0, readParam)
+  ImageIO.write(image, "png", to)
+  Logger.trace { "Convert successfully" }
+}
