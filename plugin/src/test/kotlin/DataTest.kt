12f70e2a6a060a9c28443f2a4ec5ba0c6d637740
itsusinn <itsusinn@foxmail.com>
2021-10-17 15:09:22 +0800

    test: add unit tests

---
+++ plugin/src/test/kotlin/DataTest.kt
@@ -0,0 +1,26 @@
+import io.ktor.util.hex
+import kotlinx.serialization.InternalSerializationApi
+import org.meowcat.mesagisto.client.Cbor
+import org.meowcat.mesagisto.client.data.Message
+import org.meowcat.mesagisto.client.data.MessageType
+import org.meowcat.mesagisto.client.data.Profile
+import org.meowcat.mesagisto.client.toByteArray
+import java.util.*
+
+object DataTest {
+  @JvmStatic fun main(args: Array<String>) {
+    testCbor()
+  }
+  @OptIn(InternalSerializationApi::class)
+  private fun testCbor() {
+    val message = Message(
+      profile = Profile(1212121L, null, null),
+      id = 2222.toByteArray(),
+      reply = 123232.toByteArray(),
+      chain = listOf(MessageType.Text("this is text 1")),
+    )
+    val bytes = Cbor.encodeToByteArray(message)
+    println(hex(bytes))
+    Cbor.decodeFromByteArray<Message>(bytes)
+  }
+}
--- plugin/src/test/kotlin/RunMirai.kt
+++ plugin/src/test/kotlin/RunMirai.kt
@@ -8,11 +8,10 @@ import net.mamoe.mirai.console.util.ConsoleExperimentalApi
 
 @ConsoleExperimentalApi
 suspend fun main() {
+  MiraiConsoleTerminalLoader.startAsDaemon()
 
-   MiraiConsoleTerminalLoader.startAsDaemon()
+  Plugin.load()
+  Plugin.enable()
 
-   MesagistoPlugin.load()
-   MesagistoPlugin.enable()
-
-   MiraiConsole.job.join()
+  MiraiConsole.job.join()
 }
