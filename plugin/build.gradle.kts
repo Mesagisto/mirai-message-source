1885b914cc40551b2b783c9478730917bbb73c8f
itsusinn <itsusinn@foxmail.com>
2021-10-17 15:06:36 +0800

    build: bump dependencies

--- plugin/build.gradle.kts
+++ plugin/build.gradle.kts
@@ -1,22 +1,37 @@
 plugins {
-   kotlin("jvm")
-   java
-   id("com.github.johnrengelman.shadow")
-   id("net.mamoe.mirai-console")
+  kotlin("jvm")
+  java
+  id("com.github.johnrengelman.shadow")
+  kotlin("plugin.serialization")
+  id("net.mamoe.mirai-console")
 }
+repositories {
+  maven("https://jitpack.io")
+  mavenCentral()
+}
+
 mirai {
-   coreVersion = "2.6.5"
+  coreVersion = "2.8.0-M1"
   jvmTarget = JavaVersion.VERSION_11
+  excludeDependency("org.jetbrains.kotlin:kotlin-stdlib")
+  excludeDependency("org.jetbrains.kotlin:kotlin-reflect")
+  excludeDependency("org.jetbrains.kotlin:kotlin-stdlib-common")
+  excludeDependency("org.jetbrains:annotations")
 }
 dependencies {
-   implementation("org.jetbrains.kotlin:kotlin-stdlib:1.4.30")
-   implementation("org.meowcat:handy-dandy:0.1.0")
+  implementation("io.arrow-kt:arrow-core:1.0.0")
+  implementation("io.nats:jnats:2.12.0")
+  implementation("org.rocksdb:rocksdbjni:6.22.1.1")
+  implementation("com.github.gotson:webp-imageio:0.2.2")
+  implementation("org.tinylog:tinylog-impl:2.4.0-M1")
+  implementation("org.tinylog:tinylog-api-kotlin:2.4.0-M1") {
+    isTransitive = false
+  }
+  compileOnly("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+  compileOnly("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.5.1")
+  implementation("org.jetbrains.kotlinx:kotlinx-serialization-cbor:1.3.0")
+  implementation("org.jetbrains.kotlinx:kotlinx-serialization-protobuf:1.3.0")
+  implementation("org.meowcat:mesagisto-client-jvm:1.0.2")
 
-   implementation("io.nats:jnats:2.10.0")
-
-   // jackson
-   implementation("com.fasterxml.jackson.core:jackson-core:2.12.1")
-   implementation("com.fasterxml.jackson.core:jackson-databind:2.12.1")
-   implementation("com.fasterxml.jackson.core:jackson-annotations:2.12.1")
-   implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.12.1")
+  testCompileOnly("junit:junit:4.13.2")
 }
