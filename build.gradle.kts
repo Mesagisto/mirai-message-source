9432c8cc18fd19b95a4628becc837125b0eac699
Itsusinn <itsusinn@meowcat.org>
2022-07-04 12:54:06 +0800

    chore: update mirai and mesagisto-client

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,10 +3,11 @@ plugins {
   kotlin("jvm") version "1.6.0"
   id("com.github.johnrengelman.shadow") version "6.0.0"
   kotlin("plugin.serialization") version "1.6.0"
-  id("net.mamoe.mirai-console") version "2.12.0-RC"
+  id("net.mamoe.mirai-console") version "2.12.0"
 }
 group = "org.meowcat"
 version = "1.3.0"
+
 tasks.compileKotlin {
   kotlinOptions {
     jvmTarget = "1.8"
@@ -20,7 +21,7 @@ repositories {
   mavenLocal()
 }
 mirai {
-  coreVersion = "2.12.0-RC"
+  coreVersion = "2.12.0"
   jvmTarget = JavaVersion.VERSION_1_8
 }
 dependencies {
@@ -30,7 +31,7 @@ dependencies {
   compileOnly("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
   compileOnly("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.0")
   compileOnly("org.jetbrains.kotlinx:kotlinx-serialization-protobuf:1.2.2")
-  implementation("org.mesagisto:mesagisto-client:1.5.0")
+  implementation("org.mesagisto:mesagisto-client:1.5.1")
   testImplementation("junit:junit:4.13.2")
   testImplementation("org.hamcrest:hamcrest:2.2")
 }
