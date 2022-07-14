bd76fad88635d89b73a952f828e9c8a2cbd5537c
Itsusinn <itsusinn@meowcat.org>
2022-07-14 18:04:10 +0800

    refactor(perm): allow operator execute commands

--- src/main/kotlin/Command.kt
+++ src/main/kotlin/Command.kt
@@ -1,7 +1,7 @@
 package org.meowcat.mesagisto.mirai
 
 import net.mamoe.mirai.contact.Member
-import net.mamoe.mirai.contact.isOwner
+import net.mamoe.mirai.contact.isOperator
 import net.mamoe.mirai.event.events.GroupMessageEvent
 import net.mamoe.mirai.message.data.MessageSource.Key.quote
 import org.meowcat.mesagisto.mirai.handlers.Receive
@@ -14,6 +14,9 @@ object Command {
     if (Config.perm.strict) {
       if (!Config.perm.users.contains(event.sender.id)) return@run
     }
+    // 如果不是群主或管理员, 则跳出处理流程
+    if (!event.sender.isOperator()) return@run
+
     val text = event.message.contentToString()
     if (!text.startsWith("/信使") and !text.startsWith("/f")) return
     val args = text.split(" ")
@@ -46,11 +49,6 @@ object Command {
   }
 
   private suspend fun Member.bindChannel(channel: String?) {
-    if (!isOwner()) {
-      group.sendMessage("您不是群主,无法绑定信使频道")
-      return
-    }
-
     val address = channel ?: group.id.toString()
     if (Config.bindings.put(group.id, address) != null) {
       Receive.change(group.id, address)
@@ -61,10 +59,6 @@ object Command {
     }
   }
   private suspend fun Member.unbindChannel() {
-    if (!isOwner()) {
-      group.sendMessage("您不是群主,无法解绑信使频道")
-      return
-    }
     Config.bindings.remove(group.id)
     Receive.del(group.id)
     group.sendMessage("已解绑本群的信使频道")
