1d66b26a84ba3aa2d471517960502db2515939af
Itsusinn <itsusinn@meowcat.org>
2023-01-06 18:35:17 +0800

    ci: change release name

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@ build(){
 	./gradlew clean buildPlugin
 	rm -rf packages
 	mkdir -p packages
-	mv build/mirai/*.mirai2.jar packages/mesagisto.mirai2.jar
+	mv build/mirai/*.mirai2.jar packages/mirai-message-source.mirai2.jar
 }
 build
 
